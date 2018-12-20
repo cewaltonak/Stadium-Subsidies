aa04922a51bf3b9aafc11766d1a8eaa83e47cfd1
cewaltonak <cewalton@questu.ca>
2018-12-20 05:55:41 -0800

    Update main.R

--- main.R
+++ main.R
@@ -1,8 +1,10 @@
-getwd()
+getwd() # check working directory
 
-w.dir <- getwd()
+w.dir <- getwd() # link working directory to a manipulable object
 
-output.folder.names <- c(
+library(ggplot2)
+
+output.folder.names <- c( # handy dandy replicable file names
   "raw data",
   "clean data",
   "cleaners",
@@ -35,7 +37,7 @@ p.figures <- paste(w.dir, (output.folder.names[5]), sep ="/")
 # Build a test graph to check linear regression
 test.data.linear <- function(a, b, sd.noise, x){
   
-  y <- a * x + b
+  y <- a * x + b # your basic bare-bones "y = mx + b" slope-intercept formula
   
   noise <- rnorm(length(x), 0, sd.noise)
   y.n <- y + noise
@@ -54,13 +56,13 @@ plot(d.1)
 # Build logarithmic function
 test.data.log <- function(b, sd.noise, x){
   
-  y <- log(x, base = exp(1)) + b
+  y <- log(x, base = exp(1)) + b # base log graph, the anti-parabola
   
   noise <- rnorm(length(x), 0, sd.noise)
   y.n <- y + noise
 }
 
-y.sim.2 <- test.data.log(1, 1, x.sim)
+y.sim.2 <- test.data.log(1, 1, x.sim) # save the values to a specific graph
 
 d.2 <- data.frame("Projected Cost ($millions)" = x.sim, RoI = y.sim.2)
 
@@ -69,7 +71,7 @@ plot(d.2) # SUCCESS
 # plot explonential function
 test.data.exp <- function(b, sd.noise, x){
   
-  y <- x ^ 2 + b
+  y <- x ^ 2 + b # base formula for a parbolic graph
   
   noise <- rnorm(length(x), 0, sd.noise)
   y.n <- y + noise
@@ -96,15 +98,29 @@ str(t.1$Net.Revenue)
 
 # remove rows with missing values from dataset
 t.2 <- t.1[complete.cases(t.1), ]
-str(t.2)
+str(t.2) # check the structure, make sure nothing fell out in transit
 
 # visualize initial dataset
 plot(x = t.2$Subsidy, y = t.2$Net.Revenue, xlab = "Subsidy", ylab = "Net Return")
 
-t.lm <- lm(t.2$Net.Revenue~t.2$Subsidy, data = t.2)
+t.lm <- lm(t.2$Net.Revenue~t.2$Subsidy, data = t.2) # assign new 
 plot(t.lm)
 
+summary(t.lm) # get a rundown of what I just made
+coef(t.lm) # check the default correlation coefficients
+anova(t.lm) # and run an ANOVA test because why not, we can
 
+ggplot(t.2, aes(y = t.2$Net.Revenue, x = t.2$Subsidy)) + # get on that GGplot train boiiii
+  geom_point(size = 2, col = "red") + # color the datapoints
+  geom_smooth(method = "lm", se = TRUE) + 
+  theme(aspect.ratio = 0.80)
 
+confint(t.lm, level = 0.95) # exmine the lm plot's confidence intervals
 
+anova(t.lm)
 
+??pearson
+cor.test(x = t.2$Subsidy, y = t.2$Net.Revenue,
+         alternative = c("two.sided"),
+         method = c("pearson"),
+         exact = NULL, conf.level = 0.95, continuity = FALSE)
